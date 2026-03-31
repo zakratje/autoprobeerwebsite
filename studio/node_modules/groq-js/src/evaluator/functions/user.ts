006c592c334cf27ee70b1e73d9ef47afad0b8b04
zakratje <klosterstommy@gmail.com>
2026-03-31 20:17:06 +0200

    Cleaner: node_modules verwijderd voor een snellere website

--- studio/node_modules/groq-js/src/evaluator/functions/user.ts
+++
@@ -1,8 +0,0 @@
-import {constantExecutor} from '../evaluate'
-import type {FunctionSet} from '.'
-
-const user: FunctionSet = {}
-user['attributes'] = constantExecutor(() => {
-  throw new Error('not implemented')
-})
-export default user
